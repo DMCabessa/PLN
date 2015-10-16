09085e466cb21e73fbb4206a6bc98eaf2c35c160
joaogsma <joaogsma@gmail.com>
2015-10-16 13:22:34 -0300

    Once uppon a time

---
+++ src/script_bagofwords.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup,SoupStrainer
+import nltk
+categories = {
+    'earn': "", 
+    'acquisitions': "",
+    'money-fx': "",
+    'grain': "", 
+    'crude': "",
+    'trade': "", 
+    'interest': "",
+    'ship': "",
+    'wheat': "",
+    'corn': ""
+    }
+trainList = []
+text = ""
+testList = []
+for i in range(0,1):
+    filename = "reut2-0" + ("0" if i < 10 else "") + str(i) + ".sgm"
+    f = open(filename)
+    data = f.read()
+    soup = BeautifulSoup(data)
+    reuters = soup.findAll('reuters')
+    for reuter in reuters:
+	if reuter['lewissplit'] == "train" or reuter['lewissplit'] == "TRAIN":
+            topic = reuter.find('topics')
+            ds = topic.findAll('d')
+            useText = False
+            for d in ds:
+                if d.text in categories.keys():
+                    useText = True
+                    break
+            if useText and reuter.find('content') != None:
+                for d in ds:
+                    if d.text in categories.keys():
+                        categories[d.text] += reuter.find('content').text
+                trainList.append(reuter.find('content').text)
+                text = text + reuter.find('content').text
+                #print reuter.find('content').text + "\n"
+#print trainList
