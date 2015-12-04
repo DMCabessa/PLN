6db90b51f8394a82b7e9658e65b727b15863d859
joaogsma <joaogsma@gmail.com>
2015-12-04 17:38:56 -0300

    finished removing old files from IR

--- IR/term_data.py
+++
@@ -1,9 +0,0 @@
-class TermData:
-	def __init__(self, frequency = 0, posting_list = []):
-		self.frequency = frequency
-		self.posting_list = posting_list
-
-class Posting:
-	def __init__(self, doc_id, positions = []):
-		self.doc_id = doc_id
-		self.positions = positions
